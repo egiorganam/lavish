baee4e7f5f62c58353103800546359608987d08e
egiorganam <giorgana.elih@gmail.com>
2021-05-12 22:29:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>TITLE</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem modi praesentium quam? Facere pariatur mollitia quisquam doloribus eos est iste iure minus, ducimus libero! Officiis quisquam odit quaerat. Quaerat, atque!</p>
+      </div>
     </div>
   );
 }
